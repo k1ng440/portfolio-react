b19512a5030a6b9d2f3f8e39434abe049453bc89
pavel <info@codegenie.co>
2022-05-25 13:18:35 +0600

    clean up

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -20,36 +20,6 @@ const Skills = () => {
           <SkillListItem key={skill.category} skill={skill} />
         ))}
       </ul>
-
-      <div className="text-sm font-medium text-gray-500 border-r border-gray-200 dark:text-gray-400 dark:border-gray-700">
-        <ul className="">
-          <li className="mr-2">
-            <a
-              href="#whatever"
-              className="inline-block p-4 border-l-2 hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            >
-              Profile
-            </a>
-          </li>
-          <li className="mr-2">
-            <a
-              href="#whatever"
-              className="inline-block p-4 text-blue-600 border-l-2 active dark:text-blue-500"
-              aria-current="page"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li className="mr-2">
-            <a
-              href="#whatever"
-              className="inline-block p-4 border-l-2 hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            >
-              Profile
-            </a>
-          </li>
-        </ul>
-      </div>
     </div>
   )
 }
